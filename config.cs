3a730b78c80e054b624e2e526b90ddd75c459043
mrl4an <127203643+mrl4an@users.noreply.github.com>
2023-11-26 21:51:52 +0300

    Update config.cs

--- config.cs
+++ config.cs
@@ -11,7 +11,7 @@ namespace Buff163_TelegramBot
     {
         public static string Buff_link = @"https://buff.163.com/goods/";
         public static double Percent_ForBuy = 10;//в качестве разграничителя используется запятая(например 10,1)
-        public static string Sql_con = @"server=mailoi4r.beget.tech;user=mailoi4r_plus;database=mailoi4r_plus;password=ESGnWzIIe22;";
+        public static string Sql_con = @"";
         public static double float_for_buy = 0.01;
     }
 }
